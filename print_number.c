631c53f1b7022011a4997917d29fd43a6bb20401
zinahaj <zina.haj@camous.technion.ac.il>
2022-04-10 21:48:50 +0300

    updated code

--- print_number.c
+++ print_number.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int course_number[6] = {4, 2, 1, 4, 3, 2};
+int course_number[6] = {2, 3, 4, 1, 2, 4};
 int main() {
 	for(int i = 6; i > 0; i--) {
 		printf("%d", course_number[i]);
